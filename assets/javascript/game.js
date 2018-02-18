c71ddcb952e65a15a7f39ddab4c65c49728a1cc2
Mapi Pina <mpina09@gmail.com>
2018-02-18 13:24:59 -0500

    added in skeleton of js, css, and html. will continue to define variables and add functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+// Starting this off by listing needed variables
+
+//random number displayed, score keeper, wins/losses, crystals, win/lose message all need variables
+var random;
+var wins = 0;
+var losses = 0;
+var fruit1;
+var fruit2;
+var fruit3;
+var fruit4;
+var currentScore;
+
+//below will be functions, if/else statements, and additional variables/attributes
+$(document).ready(function() {  //game will start when the document is ready
+	// body...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
