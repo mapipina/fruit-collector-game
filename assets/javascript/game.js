c430250ee146211f280f37416273cc86bfa88b99
Mapi Pina <mpina09@gmail.com>
2018-02-18 14:21:14 -0500

    started to build on click events and define variables but trying to figure out how to append the value of the fruit variables to the images without displaying it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,46 @@
 // Starting this off by listing needed variables
 
+
+
+//below will be functions, if/else statements, and additional variables/attributes
+$(document).ready(function() {  //game will start when the document is ready
 //random number displayed, score keeper, wins/losses, crystals, win/lose message all need variables
 var random;
 var wins = 0;
 var losses = 0;
-var fruit1;
-var fruit2;
-var fruit3;
-var fruit4;
-var currentScore;
+var pine;
+var avo;
+var straw;
+var ber;
+var currentScore = 0;
 
-//below will be functions, if/else statements, and additional variables/attributes
-$(document).ready(function() {  //game will start when the document is ready
-	// body...
+//random number generator. i'll have to reference it later as well so the number changes once a win/lose occurs
+	random = Math.floor(Math.random() * (101) + 19);
+	$("#randomNumber").text(random);
 
 
+//okay, I'll need a random number generator for each fruit
+	pine = Math.floor(Math.random() * (12));
+
+	avo = Math.floor(Math.random() * (12));
+	
+	straw = Math.floor(Math.random() * (12));
+
+	ber = Math.floor(Math.random() * (12));
+	
+
+//establishing on click events
+$("#pineapple").on("click", callbackPine);
+
+$("#avocado").on("click", callbackAvo);
+
+$("#strawberry").on("click", callbackStraw);
+
+$("#berry").on("click", callbackBer);
+
+
+
+//
 
 
 
